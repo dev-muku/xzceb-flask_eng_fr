99a3b35cb0e4334fc63985ac45fa3632723741b5
dev-muku <devnandwani21@gmail.com>
2022-03-11 08:00:57 +0000

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,10 @@
-import json
+"""
+Module for Language Translation
+"""
+import os
+#import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,21 +17,36 @@ language_translator = LanguageTranslatorV3(
     version='2022-03-09',
     authenticator=authenticator
 )
-
+"""
+Setting Watson Language Translator domain
+"""
 language_translator.set_service_url(url)
 
 
-def englishToFrench(englishText):
+def english_to_french(english_text):
+    """
+    Function for English to French Translation
+    """
+    if english_text is None :
+        french_text = None
+        return french_text
+
     translation = language_translator.translate(
-    text=englishText,
+    text=english_text,
     model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+    french_text = translation['translations'][0]['translation']
+    return french_text
 
 
-def frenchToEnglish(frenchText):
+def french_to_english(french_text):
+    """
+    Function for French to English Translation
+    """
+    if french_text is None :
+        english_text = None
+        return english_text
     translation = language_translator.translate(
-    text=frenchText,
+    text=french_text,
     model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
-    return englishText
+    english_text = translation['translations'][0]['translation']
+    return english_text
--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,38 @@
-from machinetranslation import translator
+'''
+Defining app routes and setting server port.
+'''
 from flask import Flask, render_template, request
-import json
+from machinetranslation import translator
+#import json
 
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+def english_2_french():
+    '''
+    Translating text from English to French.
+    '''
+    text_to_translate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translation_to_french = translator.english_to_french(text_to_translate)
+    return translation_to_french
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+def french_2_english():
+    '''
+    Translating text from French to English.
+    '''
+    text_to_translate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translation_to_english = translator.french_to_english(text_to_translate)
+    return translation_to_english
 
 @app.route("/")
-def renderIndexPage():
-    # Write the code to render template
+def render_index_page():
+    '''
+    Rendering index page.
+    '''
+    return render_template("templates/index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
